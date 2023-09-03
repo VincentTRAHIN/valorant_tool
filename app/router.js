aa9fc2fe4018e9c9ffe61e3254f81548116dad0b
Vincent Trahin <vincent.trahin@gmail.com>
2023-09-03 16:35:49 +0200

    Etape 11 terminé

--- app/router.js
+++ app/router.js
@@ -2,12 +2,55 @@ const { Router } = require("express");
 const router = Router();
 
 router.get("/", (req, res) => {
-    res.render('index');
+    const { champions } = req.app.locals;
+    const { maps } = req.app.locals;
+    const title = "Home"
+    const CSS = "index";
+    res.render('index', {title, CSS});
 });
 
 router.get("/champions", (req, res) => {
     const { champions } = req.app.locals;
-    res.render('champions');
+    const title = "Champions"
+    const CSS = "champions";
+    res.render('champions', {title, CSS});
 });
+
+router.get("/champions/:championName", (req, res) => {
+    const { champions } = req.app.locals;
+    const championName = req.params.championName;
+    const foundChampion = champions.find(champion => champion.displayName === championName)
+    const title = foundChampion.displayName;
+    const CSS = "champion";
+    if (foundChampion) {
+        res.render('champion', {foundChampion, title, CSS});
+    } else {
+        res.status(404).render("notFound");
+    }
+    
+});
+
+router.get("/maps", (req, res) => {
+    const { maps } = req.app.locals;
+    const title = "maps";
+    const CSS = "maps";
+    res.render('maps', {title, CSS});
+});
+
+router.get("/maps/:mapName", (req, res) => {
+    const { maps } = req.app.locals;
+    const mapName = req.params.mapName;
+    const foundMap = maps.find(map => map.displayName.toLowerCase() === mapName)
+    console.log(foundMap);
+    if (foundMap) {
+        const title = foundMap.displayName;
+        const CSS = "map";
+        res.render('map', {foundMap, title, CSS});
+    } else {
+        res.status(404).send("notFound");
+    }
+    
+});
+
 
 module.exports = router;
