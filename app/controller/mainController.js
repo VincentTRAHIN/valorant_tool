9c7789ec41da72bd5dc64b113c4f8d512848fdd2
Vincent Trahin <vincent.trahin@gmail.com>
2023-09-22 16:04:57 +0200

    Add some light responsive design

--- app/controller/mainController.js
+++ app/controller/mainController.js
@@ -1,12 +1,11 @@
-const mainController ={
-    
-    getHomePage: (req,res) => {
-        const { champions } = req.app.locals;
-        const { maps } = req.app.locals;
-        const title = "Home"
-        const CSS = "index";
-        res.render("index", {title, CSS});
-    }
-}
-
+/* eslint-disable linebreak-style */
+const mainController = {
+  getHomePage: (req, res) => {
+    const { champions } = req.app.locals;
+    const { maps } = req.app.locals;
+    const title = 'Home';
+    const CSS = 'index';
+    res.render('index', { title, CSS });
+  },
+};
 module.exports = mainController;
